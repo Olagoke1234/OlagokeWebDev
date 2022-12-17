259960bc51787a9838f4d63c57b5e81a9f15e324
Olagoke Olorunkosebi <olagokeolorunkosebi@mail.com>
2022-12-17 08:24:49 +0100

    Project resumed

---
+++ main.js
@@ -0,0 +1,12 @@
+windows.addEventListener("scroll", reveal);
+
+var reveals = document.querySelectorAll(".reveal");
+var i;
+
+for (i = 0; i < reveals.length; i++) {
+  if (reveals[i].getBoundingClientRect().top < window.innerHeight) {
+    reveals[i].classList.add("active");
+  } else {
+    reveals[i].classList.remove("active");
+  }
+}
